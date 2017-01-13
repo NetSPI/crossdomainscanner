f1d359e9f0a6867976536fbb158d2232cb7cc245
Jake Reynolds <jreynoldsdev@gmail.com>
2017-01-13 10:07:25 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# crossdomainScanner
+Python tool to check for expired domains still allowed in crossdomain.xml files.
+
+## Installation
+```
+~$ git clone https://github.com/JacobReynolds/crossdomainScanner.git
+~$ cd crossdomainScanner
+~$ pip install -r requirements.txt
+[follow the example below for runtime usage]
+```
+## Example:
+
+```
+~$ python scanner.py https://jakereynolds.co -v -o output.txt
+~$ cat output.txt
+Searching crossdomain.xml on https://jakereynolds.co for unregistered domains
+
+=============================================================
+
+Crossdomain contents:
+ - jakereynoldsexpireddomain.com
+ - asdaasdasfwkjhcjhbwrgkljsv.com
+ - thisisanexpireddomainaswell.es
+
+Possible expired domains:
+jakereynoldsexpireddomain.com
+```
+
+This means that https://jakereynolds.co allows http://jakereynoldsexpireddomain.com in their crossdomain.xml file.  However, the latter is not registered to any DNS.  An attacker could now buy that domain and get full cross-domain access to https://jakereynolds.co
+
+This tool is created for Ethical Hacking purposes, any illicit use is not related to its creator.
---
+++ scanner.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+import re
+import sys
+import errno
+import whois
+import socket
+import argparse
+import requests
+import subprocess
+from socket import error as socket_error
+
+#Parse command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("domain", help="Search the crossdomain.xml file on <domain>, must be fully-qualified (i.e. http[s]://)")
+parser.add_argument("-v", "--verbose", help="Print information to the console during runtime",action="store_true")
+parser.add_argument("-o", "--output", type=str,
+                    help="Write output to file")
+args = parser.parse_args()
+
+
+inputDomain = args.domain
+verbose = args.verbose
+if args.output:
+    f = open(args.output, 'w')
+    sys.stdout = f
+
+if verbose:
+    print("Searching crossdomain.xml on " + inputDomain + " for unregistered domains\n")
+    print("=============================================================\n")
+
+#Request the crossdomain file
+crossDomain = requests.get(inputDomain+"/crossdomain.xml", timeout=10)
+#Parse out all domains from the file
+domains = re.findall(r'<allow-.*-from domain="(.*)"[ ]*?[\/]?>', crossDomain.text)
+#Remove any duplicates
+domains = list(set(domains))
+possibleDomains = []
+if verbose:
+    print("Crossdomain contents: ")
+
+for domain in domains:
+    if verbose:
+        print(" - " + domain)
+    #Regex to get only the root domain or wildcard, am I missing any special characters?
+    #Currently excludes subdomains
+    domain = re.search(r'([a-zA-Z0-9-_~]+[.](?:[A-Za-z]{3,}|[A-Za-z]{2}\.[A-Za-z]{2}|[A-za-z]{2})(?:\n|$))|(^\*(?:\n|$))', domain)
+    if domain is not None:
+        domain = domain.group(0)
+        #Is there a more elegant way to do this, than check in every loop?
+        if domain == '*':
+            print("\n------------Wildcard domain detected - YARD SALE------------")
+            continue
+    	try:
+            #Run whois
+            whoisOutput = whois.whois(domain)
+            if whoisOutput.status is None:
+                raise whois.parser.PywhoisError('error')
+        except whois.parser.PywhoisError as errorOutput:
+            try:
+                #whois does not have full support (ex: .es domains), so double check false positives with socket
+                whoisOutput = socket.gethostbyname(domain)
+            except socket_error as serr:
+                #[Errno 11001] getaddrinfo failed
+                #[Errno 11004] getaddrinfo failed
+                #Most likely cause is the domain name has not been purchased
+                if serr.errno == 11001 or serr.errno == -2 or serr.errno == 11004:
+                    possibleDomains.append(domain)
+
+
+#Let the user know of any failed lookups
+if len(possibleDomains) > 0:
+    print("\nPossible expired domains: ")
+    for domain in possibleDomains:
+        print(domain)
+
+#Do some cleanup
+if args.output:
+    sys.stdout = sys.__stdout__
+    f.close()
